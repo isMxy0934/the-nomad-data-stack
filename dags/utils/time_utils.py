95b45a450e02fa5ae678cd9710b989a60ecc4bc5
isMxy0934 <ismxy@outlook.com>
2025-12-13 14:59:41 +0800

    Remove obsolete Akshare stock data extraction DAG and update utility functions for date handling. Adjust Dockerfile to use a previous version of Airflow.

--- dags/utils/time_utils.py
+++ dags/utils/time_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 
@@ -29,3 +29,31 @@ def get_current_date_str(tz='Asia/Shanghai') -> str:
     timezone = pytz.timezone(tz)
     now = datetime.now(timezone)
     return now.strftime('%Y%m%d')
+
+def get_previous_date_str(tz='Asia/Shanghai') -> str:
+    """
+    Get previous date string
+    
+    Args:
+        tz: timezone, default to Beijing timezone
+    
+    Returns:
+        str: formatted date string (yyyy-MM-dd)
+    """
+    timezone = pytz.timezone(tz)
+    previous_day = datetime.now(timezone) - timedelta(days=1)
+    return previous_day.strftime('%Y-%m-%d')
+
+def get_previous_partition_date_str(tz='Asia/Shanghai') -> str:
+    """
+    Get previous partition date string
+    
+    Args:
+        tz: timezone, default to Beijing timezone
+    
+    Returns:
+        str: formatted date string (yyyy-MM-dd)
+    """
+    timezone = pytz.timezone(tz)
+    previous_day = datetime.now(timezone) - timedelta(days=1)
+    return previous_day.strftime('%Y-%m-%d')
