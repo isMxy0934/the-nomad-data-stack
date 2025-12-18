f28724ca19a0efec285f837bf331edd84fb41ebe
isMxy0934 <ismxy@outlook.com>
2025-12-18 12:14:47 +0800

    Update trigger task in dw_start_dag.py for improved clarity and consistency
    - Refactored the task_id in the TriggerDagRunOperator to dynamically use the Trigger_DAG_ID variable, enhancing maintainability.
    - Ensured consistent naming conventions for better readability and understanding of the DAG structure.

--- dags/dw_start_dag.py
+++ dags/dw_start_dag.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
-
+Trigger_DAG_ID = "dw_catalog_dag"
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime(2024, 1, 1),
@@ -13,7 +13,7 @@ with DAG(
     tags=["orchestration"],
 ) as dag:
     trigger_catalog = TriggerDagRunOperator(
-        task_id="trigger_catalog",
-        trigger_dag_id="dw_catalog_dag",
+        task_id=f"trigger_{Trigger_DAG_ID}",
+        trigger_dag_id=Trigger_DAG_ID,
         reset_dag_run=True,
     )
