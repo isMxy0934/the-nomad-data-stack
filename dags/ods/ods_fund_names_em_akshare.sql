9d292e869f79f5457c2427ec1606d3e028ccabfa
isMxy0934 <ismxy@outlook.com>
2025-12-21 14:57:17 +0800

    Remove obsolete SQL scripts for daily stock price and fund price extraction, streamlining the codebase and enhancing maintainability.

--- dags/ods/ods_fund_names_em_akshare.sql
+++ dags/ods/ods_fund_names_em_akshare.sql
@@ -1,13 +1,9 @@
 SELECT
   symbol,
   name,
-  close,
-  high,
-  low,
-  vol,
-  amount,
-  pre_close,
-  pct_chg,
+  fund_type,
+  pinyin_full,
+  pinyin_short,
   CAST(STRPTIME(CAST(trade_date AS VARCHAR), '%Y%m%d') AS DATE) AS trade_date,
   '${PARTITION_DATE}' AS dt
-FROM tmp_ods_daily_fund_price_akshare;
+FROM tmp_ods_fund_names_em_akshare;
